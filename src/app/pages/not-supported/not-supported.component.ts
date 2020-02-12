0ab87892ffd66cd394f6c0d1a70dda0ff135d794
Ankit Tomer <ankit@benzeenautoparts.com>
2020-02-13 00:55:43 +0530

    bug fixing

--- src/app/pages/not-supported/not-supported.component.ts
+++ src/app/pages/not-supported/not-supported.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { SmsService } from 'src/app/shared/services/sms.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-not-supported',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotSupportedComponent implements OnInit {
 
-  constructor() { }
+  phone: number;
+
+  constructor(private smsService: SmsService, public toastrService: ToastrService) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    this.smsService.sendSms({ to: this.phone, message: 'Phone Chhodo Dil Jodo contest, Follow the link to register: ' + environment.baseUrl + ' Hurry, you\'re just a few steps away!' })
+      .subscribe(
+        (res) => {  
+          //console.log(res);
+          this.toastrService.success('Link sent successfully.', 'Contest');
+        },
+        (err) => {
+          this.toastrService.error(err, 'Contest');
+        }
+      );
+  }
 }
